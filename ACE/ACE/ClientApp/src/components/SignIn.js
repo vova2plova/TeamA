e91034e948ef3f605fe1625e2cde003f489a72c9
v.yakovlev <wwevovancik@gmail.com>
2021-10-10 19:38:16 +0300

    Added ref to Sign in and Log In form

---
+++ ACE/ACE/ClientApp/src/components/SignIn.js
@@ -0,0 +1,36 @@
+﻿import React, { Component } from 'react';
+
+export class SignIn extends Component {
+    static displayName = SignIn.name;
+
+
+    render () {
+        return (
+        <div className="wrapper fadeInDown">
+        <div id="formContent">
+
+            <h2 className="active"> Sign In </h2>
+            
+            <h2>
+                <a className="inactive underlineHover" href = "https://localhost:5001/SignUp">Sign Up</a> 
+            </h2>
+
+            <div className="fadeIn first">
+                    <img src="https://cdn131.picsart.com/311079241107211.png" id="icon" alt="Typical User"/>
+            </div>
+
+            <form>
+                <input type="text" id="login" className="fadeIn second" name="login" placeholder="login" />
+                <input type="text" id="password" className="fadeIn third" name="login" placeholder="password" />
+                <input type="submit" className="fadeIn fourth" value="Log In"/>
+            </form>
+
+            <div id="formFooter">
+                <a className="underlineHover" href="#">Forgot Password?</a>
+            </div>
+
+        </div>
+        </div>
+        );
+    }
+}
---
+++ ACE/ACE/ClientApp/src/components/SignUp.js
@@ -0,0 +1,33 @@
+﻿import React, { Component } from 'react';
+
+export class SignUp extends Component {
+    static displayName = SignUp.name;
+
+
+    render () {
+        return (
+            <div className="wrapper fadeInDown">
+                <div id="formContent">
+
+                    <h2>
+                        <a className="inactive underlineHover"  href = "https://localhost:5001/SignIn">Sign In</a> 
+                    </h2>
+
+                    <h2 className="active">Sign Up </h2>
+
+                    <div className="fadeIn first">
+                        <img src="https://cdn131.picsart.com/311079241107211.png" id="icon" alt="Typical User"/>
+                    </div>
+
+                    <form>
+                        <input type="text" id="login" className="fadeIn second" name="login" placeholder="login" />
+                        <input type="text" id="password" className="fadeIn third" name="login" placeholder="password" />
+                        <input type="text" id = "password" className="fadeIn third" name="login" placeholder="password" />
+                        <input type="submit" className="fadeIn fourth" value="Sign Up"/>
+                    </form> 
+
+                </div>
+            </div>
+        );
+    }
+}
