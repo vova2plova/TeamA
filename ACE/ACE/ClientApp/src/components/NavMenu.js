f1d4e837709dbe0571b44b12a0c9a9ff088ffd1e
wicheril <snajper.snajp@gmail.com>
2021-10-11 16:15:53 +0300

    WebSite design 0.1

--- ACE/ACE/ClientApp/src/components/NavMenu.js
+++ ACE/ACE/ClientApp/src/components/NavMenu.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import '../LogInForm.css';
 import './NavMenu.css';
 
 export class NavMenu extends Component {
@@ -31,10 +30,6 @@ export class NavMenu extends Component {
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
-                <form className="form-inline my-2 my-lg-0">
-                  <input type="search" placeholder="Search" aria-label="Search"/>
-                    <button className = "btn btn-outline-primary" type="submit">Search</button>
-                </form>
                 <NavItem>
                   <NavLink tag={Link} className="text-dark" to="/SignIn">Sign In</NavLink>
                 </NavItem>
