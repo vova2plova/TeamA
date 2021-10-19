048383703b1ea7ead805a07208d3153a9a22964b
v.yakovlev <wwevovancik@gmail.com>
2021-10-19 16:43:37 +0300

    add user Icons

--- ACE/ACE/ClientApp/src/components/PostItem.js
+++ ACE/ACE/ClientApp/src/components/PostItem.js
@@ -6,10 +6,12 @@ export class PostItem extends Component {
 render() {
     return(
         <div id = "post">
-            <span>vova2plova</span>
+            <span>
+                <img className="iconHolder" src = "https://cdn-icons.flaticon.com/png/512/506/premium/506134.png?token=exp=1634651712~hmac=f2a2ce05638210e4210de284dee8e71a" alt = "User Icon"/>
+                vova2plova</span>
             <h1 className="tittle">Заголовок</h1>
             <div className="description">
-            <img className="imgHolder" src = "https://hsto.org/getpro/habr/upload_files/55b/90f/8f4/55b90f8f4db5298ef69452264b82be55.jpg" alt = "Photo"/>
+            <img className="imgHolder" src = "https://hsto.org/getpro/habr/upload_files/d14/f61/d6d/d14f61d6da9a3a04538e36615670b6c2.png" alt = "Photo"/>
                 Какой-то длинный текст</div>
         </div>
     );
